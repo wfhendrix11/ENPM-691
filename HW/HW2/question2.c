7722e993bfbb0a5f138b88972af80e72fc445684
Will Hendrix <wfhendrix11@gmail.com>
2021-04-05 20:49:48 -0400

    HW2 re attempt

--- HW/HW2/question2.c
+++ HW/HW2/question2.c
@@ -12,30 +12,9 @@ int encode(int a, int b, int c, int d) {
 	int y = a - d;
 	int z = x >> 31;
 
-	return y | z;
+	return z ^ y;
 }
 
-// x ^ y;
-
-/*
-   0x080483f5 <+0>:	push   %ebp
-   0x080483f6 <+1>:	mov    %esp,%ebp
-   0x080483f8 <+3>:	sub    $0xc,%esp
-   0x080483fb <+6>:	mov    0xc(%ebp),%edx
-   0x080483fe <+9>:	mov    0x10(%ebp),%eax
-   0x08048401 <+12>:	add    %edx,%eax
-   0x08048403 <+14>:	mov    %eax,-0x4(%ebp)
-   0x08048406 <+17>:	mov    0x8(%ebp),%eax
-   0x08048409 <+20>:	sub    0x14(%ebp),%eax
-   0x0804840c <+23>:	mov    %eax,-0x8(%ebp)
-   0x0804840f <+26>:	mov    -0x4(%ebp),%eax
-   0x08048412 <+29>:	sar    $0x1f,%eax
-   0x08048415 <+32>:	mov    %eax,-0xc(%ebp)
-   0x08048418 <+35>:	mov    -0x4(%ebp),%eax
-   0x0804841b <+38>:	xor    -0x8(%ebp),%eax
-   0x0804841e <+41>:	leave  
-   0x0804841f <+42>:	ret 
-*/
 
 // x ^ z
 
@@ -56,8 +35,7 @@ int encode(int a, int b, int c, int d) {
    0x08048418 <+35>:	mov    -0x4(%ebp),%eax
    0x0804841b <+38>:	xor    -0xc(%ebp),%eax
    0x0804841e <+41>:	leave  
-   0x0804841f <+42>:	ret 
-*/
+   0x0804841f <+42>:	ret  
 
 // z ^ y
 
@@ -78,27 +56,7 @@ int encode(int a, int b, int c, int d) {
    0x08048418 <+35>:	mov    -0xc(%ebp),%eax
    0x0804841b <+38>:	xor    -0x8(%ebp),%eax
    0x0804841e <+41>:	leave  
-   0x0804841f <+42>:	ret  
+   0x0804841f <+42>:	ret     
 */
 
-// y | z
 
-/*
-   0x080483f5 <+0>:	push   %ebp
-   0x080483f6 <+1>:	mov    %esp,%ebp
-   0x080483f8 <+3>:	sub    $0xc,%esp
-   0x080483fb <+6>:	mov    0xc(%ebp),%edx
-   0x080483fe <+9>:	mov    0x10(%ebp),%eax
-   0x08048401 <+12>:	add    %edx,%eax
-   0x08048403 <+14>:	mov    %eax,-0x4(%ebp)
-   0x08048406 <+17>:	mov    0x8(%ebp),%eax
-   0x08048409 <+20>:	sub    0x14(%ebp),%eax
-   0x0804840c <+23>:	mov    %eax,-0x8(%ebp)
-   0x0804840f <+26>:	mov    -0x4(%ebp),%eax
-   0x08048412 <+29>:	sar    $0x1f,%eax
-   0x08048415 <+32>:	mov    %eax,-0xc(%ebp)
-   0x08048418 <+35>:	mov    -0x8(%ebp),%eax
-   0x0804841b <+38>:	or     -0xc(%ebp),%eax
-   0x0804841e <+41>:	leave  
-   0x0804841f <+42>:	ret  
-*/
